74a1a0998163ce62a7e0735f563a28a97338c141
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-04 17:16:28 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Johanna Liu
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates volume of a sphere.
+ */
+function calculate () {
+  // input
+  const radius = parseInt(document.getElementById("radius-of-pyramid").value)
+
+  // process
+  const volume = ( 4/3 * Math.PI * Math.pow(radius, 3)
+
+  // output
+  document.getElementById("answers").innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+}
